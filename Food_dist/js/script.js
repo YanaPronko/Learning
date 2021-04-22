688470e80b500c993f272bf22810fb8ac814d1d7
YanaPronko <79938117+YanaPronko@users.noreply.github.com>
2021-04-23 00:09:54 +0300

    FOOD_DIST_SERVER

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -103,20 +103,21 @@ window.addEventListener("DOMContentLoaded", () => {
         modal = document.querySelector(".modal");
 
     function openModal () {
-        modal.classList.toggle("show");
-        document.body.style.overflow = "hidden";
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+       document.body.style.overflow = "hidden";
         // clearInterval(modalTimerId);
     }
 
     modalTrigger.forEach(item => {
         item.addEventListener("click", openModal);
-            /* modal.classList.add("show");
-            modal.classList.remove("hide"); */
+          
     });
         
     function closeModal () {
-    modal.classList.toggle("show");
-    document.body.style.overflow = "";
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+       document.body.style.overflow = "";
 }
 
     
@@ -188,83 +189,111 @@ window.addEventListener("DOMContentLoaded", () => {
             this.parent.append(element);
         }
     } 
-    new MenuCard(
-        "img/tabs/vegy.jpg", 
-        "vegy",
-        "Меню 'Фитнес'",
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container"
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    ).render();
+        if(!res.ok) {
+            new Error (`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg", 
-        "elite",
-        "Меню “Премиум'",
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        ".menu .container"
-    ).render();
+        return await res.json();
+    };
 
-    new MenuCard(
-        "img/tabs/post.jpg", 
-        "post",
-        "Меню 'Постное'",
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        ".menu .container"
-    ).render();
+  /*   getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price})  => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+
+        });
+     */
+       /*  getResource('http://localhost:3000/menu')
+            .then(data => createCard(data));
+
+        function createCard(data) {
+            data.forEach(({img, altimg, title, descr, price})  => {
+                const element = document.createElement("div");
+                element.classList.add("menu__item");
+                price = price*30;
+
+                element.innerHTML = `  
+                    <img src=${img} alt=${altimg}>
+                    <h3 class="menu__item-subtitle">${title}"</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>`
+                ;
+
+                document.querySelector(".menu .container").append(element);
+            });
+        } */
+
+        axios.get('http://localhost:3000/menu')
+            .then(data => {
+                data.data.forEach(({img, altimg, title, descr, price})  => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+                });
+            });
     
     //Form
     const forms = document.querySelectorAll("form");
 
     const message = {
-        loading: "Загрузка",
+        loading: "img/form/spinner.svg",
         success: "Спасибо! Скоро мы с вами свяжемся",
         failure: "Что-то пошло не так...",
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+                headers: 
+                        {'Content-type' : 'application/json'},
+                body:data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement("div");
-            statusMessage.classList.add("status");
-            statusMessage.textContent = message.loading;
-            form.append (statusMessage);
-
-            const request = new XMLHttpRequest();
-            request.open ("POST", "server.php");
-            request.setRequestHeader("Content-type", "application/json");
-            const formData =  new FormData(form);
-
-            const object = {};
-            formData.forEach(function (value, key){
-                object[key] = value;
-            });
-
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener("load", () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                
-                }else {
-                    showThanksModal(message.failure);
-                }
- 
-            });
+            const statusMessage = document.createElement("img");
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
             
+            form.insertAdjacentElement("afterend", statusMessage);
+        
+            
+            const formData =  new FormData(form);
+           const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            
+
+
+
+            postData( 'http://localhost:3000/requests', JSON.stringify(json))
+                .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally (()=> {
+                form.reset();
+            });
+                                
         });
     }
 
@@ -275,7 +304,7 @@ window.addEventListener("DOMContentLoaded", () => {
         openModal();
 
         const thanksModal = document.createElement("div");
-        thanksModal.classList.add (".modal__dialog");
+        thanksModal.classList.add ("modal__dialog");
         thanksModal.innerHTML = `
             <div class="modal__content"> 
                 <div class="modal__close" data-close>×</div>
@@ -293,4 +322,56 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 4000);
     }
 
+    // Slider
+
+    const slides = document.querySelectorAll (".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current");
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex  = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = "none");
+        slides[slideIndex -1].style.display = "block";
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        
+        } else {
+            current.textContent = slideIndex;
+        }
+    
+    
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener("click", () => {
+        plusSlides(1);
+    });
 });
